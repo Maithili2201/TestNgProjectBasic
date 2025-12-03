b98a0484cbc6ded103047eb95f2cb01a52421fcc
maithupatil <maithilip504@gmail.com>
2025-12-03 18:05:27 +0530

    first commit

---
+++ src/test/java/package1/ClassOne.java
@@ -0,0 +1,15 @@
+package package1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+
+public class ClassOne {
+	
+			WebDriver driver;
+			WebDriverWait wait ;
+			
+}
+			
+	
